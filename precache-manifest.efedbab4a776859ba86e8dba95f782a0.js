df5f67e232fb802cee55dd03ef51bc05638bcea7
r0o0 <rayeoh7@gmail.com>
2019-07-09 00:32:13 +0900

    Updates

---
+++ precache-manifest.efedbab4a776859ba86e8dba95f782a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ab338e10981d4a4d493fafc606d2d57",
+    "url": "/simpleTodo-react/index.html"
+  },
+  {
+    "revision": "63eae53637b51dd32aa7",
+    "url": "/simpleTodo-react/static/css/main.1661b9cb.chunk.css"
+  },
+  {
+    "revision": "fecd0a957d14e024456e",
+    "url": "/simpleTodo-react/static/js/2.715d8d02.chunk.js"
+  },
+  {
+    "revision": "63eae53637b51dd32aa7",
+    "url": "/simpleTodo-react/static/js/main.fd928fd3.chunk.js"
+  },
+  {
+    "revision": "1bcef6893e8c1b33fe7a",
+    "url": "/simpleTodo-react/static/js/runtime~main.38fa1ea7.js"
+  }
+]);
